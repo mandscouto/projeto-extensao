52167c1edb5f586a88462d9928b365b26d91cd29
Amanda Couto <mandscouto@outlook.com>
2021-10-03 20:54:17 -0300

    Update script.js

--- training/JS/script.js
+++ training/JS/script.js
@@ -10,5 +10,9 @@ function somar(a, b){
 document.querySelector("#calcular").addEventListener("click", function(){
     let valorA = document.querySelector("#valorA").value;
     let valorB = document.querySelector("#valorB").value;
-    alert(parseInt(valorA)+parseInt(valorB));
+    if(valorA.length>0 && valorB.length>0){
+        alert(parseInt(valorA)+parseInt(valorB));
+    }else{
+        alert("Insira os valores a serem somados.");
+    }
 });
