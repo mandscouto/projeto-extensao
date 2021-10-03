b41a094bbb5dc4b1b97026c40badc3807e47839c
Amanda Couto <mandscouto@outlook.com>
2021-10-03 20:48:25 -0300

    Update script.js

--- training/JS/script.js
+++ training/JS/script.js
@@ -1,8 +1,12 @@
 /* var resposta = confirm("Deseja excluir?");
 console.log(resposta); */
-var a = 10;
-var b = 33;
+const a = 10;
+const b = 33;
 function somar(a, b){
     return a + b;
 }
-console.log(somar(2,5));
+//console.log(somar(2,5));
+
+document.querySelector("#calcular").addEventListener("click", function(){
+    alert("Olá mundo!");
+});
