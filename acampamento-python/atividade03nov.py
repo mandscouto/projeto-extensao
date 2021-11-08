57ea001f5f9c48da508adee2be53fe094769b620
Amanda Couto <mandscouto@outlook.com>
2021-11-07 21:15:05 -0300

    Create atividade03nov.py

---
+++ acampamento-python/atividade03nov.py
@@ -0,0 +1,10 @@
+import math
+
+n1 = int(input("nsira a nota alcançada na primeira avaliação: "))
+n2 = int(input("nsira a nota alcançada na segunda avaliação: "))
+n3 = int(input("nsira a nota alcançada na terceira avaliação: "))
+media = (n1+n2+n3)/3
+if(media<70):
+    print("Sua média é {0:.1f}".format(media)," e você está reprovado.")
+else:
+    print("Sua média é {0:.1f}".format(media)," e você está aprovado.")
