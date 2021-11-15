3410aadbb34bc546185cdb43535261d5cf486e6e
Amanda Couto <mandscouto@outlook.com>
2021-11-15 09:29:52 -0300

    Update atividade16nov.py

--- acampamento-python/atividade16nov.py
+++ acampamento-python/atividade16nov.py
@@ -1,10 +1,15 @@
 from random import randint
 jogos = ('Pedra', 'Papel', 'Tesoura')
 print('''Suas opções: 
-[0] Pedra
-[1] Papel
-[2] Tesoura''')
+[r] Pedra
+[p] Papel
+[r] Tesoura''')
 jog1 = input('Jogada: ')
 jog2 = input('Jogada: ')
-if (jog1.lower == 'x' || jog2.lower == 'x'):
-    print('Jogo encerrado.') 
+while (jog1.lower() != 'x' or jog2.lower() != 'x'):
+    if(jog1 != 'r' or jog1 != 'p' or jog1 != 't' or jog2 != 'r' or jog2 != 'p' or jog2 != 't'):
+        print('Essa jogada é inválida. Tente novamente.')
+    elif(jog1 == jog2):
+        print('Deu empate! Vamos de novo...')
+jog1 = input('Jogada: ')
+jog2 = input('Jogada: ')
