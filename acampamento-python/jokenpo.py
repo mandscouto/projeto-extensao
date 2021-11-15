4b72ac7af9f46092532b34e72797fc63330efe45
Amanda Couto <mandscouto@outlook.com>
2021-11-15 09:35:13 -0300

    Update jokenpo.py

--- acampamento-python/jokenpo.py
+++ acampamento-python/jokenpo.py
@@ -1,15 +1,43 @@
-from random import randint
-jogos = ('Pedra', 'Papel', 'Tesoura')
-print('''Suas opções: 
-[r] Pedra
-[p] Papel
-[r] Tesoura''')
-jog1 = input('Jogada: ')
-jog2 = input('Jogada: ')
-while (jog1.lower() != 'x' or jog2.lower() != 'x'):
-    if(jog1 != 'r' or jog1 != 'p' or jog1 != 't' or jog2 != 'r' or jog2 != 'p' or jog2 != 't'):
-        print('Essa jogada é inválida. Tente novamente.')
-    elif(jog1 == jog2):
-        print('Deu empate! Vamos de novo...')
-jog1 = input('Jogada: ')
-jog2 = input('Jogada: ')
+import random
+
+jogadas = ["pedra", "papel", "tesoura"]
+
+pontos_pc = 0
+pontos_jog = 0
+
+print("Pedra, papel e tesoura\n\n")
+jog = ""
+
+while jog.lower() != "sair":
+    jog = input("Qual a sua jogada?")
+    if jog in jogadas:
+        pc = random.choice(jogadas)
+        print(f"Computador jogou: {pc}\n")
+
+        # pedra ganha de tesoura
+        if jog == "pedra" and pc == "tesoura":
+            print("Você ganhou!")
+            pontos_jog = pontos_jog + 1
+        elif pc == "pedra" and jog == "tesoura":
+            print("O computador ganhou!")
+            pontos_pc = pontos_pc + 1
+        
+        # tesoura ganha de papel
+        if jog == "tesoura" and pc == "papel":
+            print("Você ganhou!")
+            pontos_jog = pontos_jog + 1
+        elif pc == "tesoura" and jog == "papel":
+            print("O computador ganhou!")
+            pontos_pc = pontos_pc + 1
+
+        # papel ganha de pedra        
+        if jog == "papel" and pc == "pedra":
+            print("Você ganhou!")
+            pontos_jog = pontos_jog + 1
+        elif pc == "papel" and jog == "pedra":
+            print("O computador ganhou!")
+            pontos_pc = pontos_pc + 1
+
+        print(f"\n\nVocê {pontos_jog} x {pontos_pc} Computador")
+    elif jog.lower() != "sair":
+        print(f"Escolha uma das opções: {jogadas}")
